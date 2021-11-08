2384c7200676be1891b540a96c615847868e4e3f
dtyagi007 <dtyagi007@users.noreply.github.com>
2021-11-08 13:19:01 -0800

    add deployment pipeline to replace stackery

---
+++ macs-deployment/lib/macs-deployment-stack.ts
@@ -0,0 +1,55 @@
+import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as iam from '@aws-cdk/aws-iam';
+
+export class MACSDeploymentStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, `MACSDeploymentArtifacts-${cdk.Stack.of(this).region}`, {
+      versioned: true
+    });
+
+    // The code that defines your stack goes here
+    new codebuild.Project(this, 'MACSDeployment', {
+      projectName: "macs-core-deployment",
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_3
+      },
+      role: iam.Role.fromRoleArn(
+        this,
+        "code-build-project-role",
+        "arn:aws:iam::025658654491:role/CodeBuild-build-promotion-snd"
+      ),
+      source: codebuild.Source.gitHub({
+        owner: "mutual-of-enumclaw",
+        repo: "macs-core",
+        webhook: true,
+        webhookFilters: [
+          codebuild.FilterGroup
+          .inEventOf(codebuild.EventAction.PUSH)
+          .andBranchIs('codebuild-deploy')
+        ]
+      }),
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'npm ci',
+              'npm run build',
+              'npm run deploy'
+            ],
+          },
+        },
+      }),
+      artifacts: codebuild.Artifacts.s3({
+        bucket,
+        includeBuildId: true,
+        packageZip: true,
+        path: 'macs/core/snd'
+      }),
+    });
+  }
+}
